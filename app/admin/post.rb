277c9f8a9bc4de762c8c79df09c5935600798554
Ala Arab <ala.arab@admenergy.com>
2019-05-06 10:57:30 -0700

    first commit

---
+++ app/admin/post.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Post do
+
+	permit_params :comment
+
+	controller do
+    def show
+      @post = Post.find(params[:id])
+      show! 
+    end
+	end
+
+	show do |post|
+		active_admin_comments
+	end
+
+end
